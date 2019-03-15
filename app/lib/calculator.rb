d9c96e645ae8378f0d8e31e10872254156352487
Tim <timbinous@gmail.com>
2019-03-14 20:38:20 -0400

    adds dividing

--- app/lib/calculator.rb
+++ app/lib/calculator.rb
@@ -3,6 +3,7 @@ class Calculator
     def calculate(calculating_string)
       calculating_string.gsub!(/\s+/, '')
       multiply calculating_string
+      divide calculating_string
     end
 
     def multiply(calculating_string)
@@ -17,6 +18,20 @@ class Calculator
 
     def calculate_multiply(match)
       match.to_s.split('*').map(&:to_i).inject(:*).to_s
+    end
+
+    def divide(calculating_string)
+      regex = /([\d]+\/[\d]+)/
+      loop do
+        break if calculating_string.exclude?('/')
+        calc = calculate_divide(calculating_string.match(regex))
+        calculating_string.sub!(regex, calc)
+      end
+      calculating_string
+    end
+
+    def calculate_divide(match)
+      match.to_s.split('/').map(&:to_i).inject(:/).to_s
     end
   end
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -4,11 +4,21 @@ describe Calculator do
   describe "#calculate" do
     context "multiplying" do
       it 'multiplies 2 numbers and injects the result into the string' do
-        expect(Calculator.calculate('10*10 + 5')).to eq('100+5')
+        expect(Calculator.multiply('10*10+5')).to eq('100+5')
       end
 
       it 'multiplies 3 numbers and injects the result into the string' do
-        expect(Calculator.calculate('10*10+5*10')).to eq('100+50')
+        expect(Calculator.multiply('10*10+5*10')).to eq('100+50')
+      end
+    end
+
+    context "dividing" do
+      it 'divides 2 numbers and injects the result into the string' do
+        expect(Calculator.divide('10/10+5')).to eq('1+5')
+      end
+
+      it 'divides 3 numbers and injects the result into the string' do
+        expect(Calculator.divide('10/10+20/10')).to eq('1+2')
       end
     end
   end
