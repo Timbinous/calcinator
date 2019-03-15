be672c2b4aaf20d7f714cc6b172f438a646ee117
Tim <timbinous@gmail.com>
2019-03-14 21:16:29 -0400

    added tests from the exercise Overlooked decimal places and added a fix for that

--- app/lib/calculator.rb
+++ app/lib/calculator.rb
@@ -2,24 +2,29 @@ class Calculator
   class << self
     def calculate(calculation_string)
       calculation_string.gsub!(/\s+/, '')
-      loop_calculation_string(/([\d]+\*[\d]+)/, '*', calculation_string)
-      loop_calculation_string(/([\d]+\/[\d]+)/, '/', calculation_string)
-      loop_calculation_string(/([\d]+\+[\d]+)/, '+', calculation_string)
-      loop_calculation_string(/([\d]+\-[\d]+)/, '-', calculation_string)
+      loop_calculation_string(/([\d\.]+\*[\d\.]+)/, '*', calculation_string)
+      loop_calculation_string(/([\d\.]+\/[\d\.]+)/, '/', calculation_string)
+      loop_calculation_string(/([\d\.]+\+[\d\.]+)/, '+', calculation_string)
+      loop_calculation_string(/([\d\.]+\-[\d\.]+)/, '-', calculation_string)
+      if calculation_string.to_i == calculation_string.to_f
+        calculation_string.to_i
+      else
+        calculation_string.to_f.round(2)
+      end
     end
 
     def loop_calculation_string(regex, operator, calculation_string)
-    loop do
-      break if calculation_string.exclude?(operator)
-      calc = calculate_operator(operator, calculation_string.match(regex))
-      calculation_string.sub!(regex, calc)
+      loop do
+        break if calculation_string.exclude?(operator)
+        calc = calculate_operator(operator, calculation_string.match(regex))
+        calculation_string.sub!(regex, calc)
+      end
+      calculation_string
     end
-    calculation_string
-  end
 
-  def calculate_operator(operator, match)
-    match.to_s.split(operator).map(&:to_i).inject(&operator.to_sym).to_s
-  end
+    def calculate_operator(operator, match)
+      match.to_s.split(operator).map(&:to_f).inject(&operator.to_sym).to_s
+    end
   end
 end
 
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -4,47 +4,57 @@ describe Calculator do
   describe "#calculate" do
     context "multiplying" do
       it 'multiplies 2 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\*[\d]+)/, '*', '10*10+5')).to eq('100+5')
+        expect(Calculator.loop_calculation_string(/([\d]+\*[\d]+)/, '*', '10*10+5')).to eq('100.0+5')
       end
 
       it 'multiplies 3 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\*[\d]+)/, '*', '10*10+5*10')).to eq('100+50')
+        expect(Calculator.loop_calculation_string(/([\d]+\*[\d]+)/, '*', '10*10+5*10')).to eq('100.0+50.0')
       end
     end
 
     context "dividing" do
       it 'divides 2 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\/[\d]+)/, '/', '10/10+5')).to eq('1+5')
+        expect(Calculator.loop_calculation_string(/([\d]+\/[\d]+)/, '/', '10/10+5')).to eq('1.0+5')
       end
 
       it 'divides 3 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\/[\d]+)/, '/', '10/10+20/10')).to eq('1+2')
+        expect(Calculator.loop_calculation_string(/([\d]+\/[\d]+)/, '/', '10/10+20/10')).to eq('1.0+2.0')
       end
     end
 
     context "adding" do
       it 'adds 2 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\+[\d]+)/, '+', '10/10+20/10')).to eq('10/30/10')
+        expect(Calculator.loop_calculation_string(/([\d]+\+[\d]+)/, '+', '10/10+20/10')).to eq('10/30.0/10')
       end
 
       it 'adds 3 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\+[\d]+)/, '+', '10+10/20+10')).to eq('20/30')
+        expect(Calculator.loop_calculation_string(/([\d]+\+[\d]+)/, '+', '10+10/20+10')).to eq('20.0/30.0')
       end
     end
 
     context "subtracting" do
       it 'subtracts 2 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\-[\d]+)/, '-', '10/20-10/10')).to eq('10/10/10')
+        expect(Calculator.loop_calculation_string(/([\d]+\-[\d]+)/, '-', '10/20-10/10')).to eq('10/10.0/10')
       end
 
       it 'subtracts 3 numbers and injects the result into the string' do
-        expect(Calculator.loop_calculation_string(/([\d]+\-[\d]+)/, '-', '10-10/20-10')).to eq('0/10')
+        expect(Calculator.loop_calculation_string(/([\d]+\-[\d]+)/, '-', '10-10/20-10')).to eq('0.0/10.0')
       end
     end
 
     context "using calculate" do
       it 'uses the correct order of operations to get the right answer' do
-        expect(Calculator.calculate('10*10/2+5*10')).to eq('100')
+        expect(Calculator.calculate('10*10/2+5*10')).to eq(100)
+      end
+    end
+
+    context "code exercise examples" do
+      it 'calculates the first example correctly' do
+        expect(Calculator.calculate('5*3+1+6/2+9*100')).to eq(919)
+      end
+
+      it 'calculates the second example correctly' do
+        expect(Calculator.calculate('5*3+1+6/85+9*100')).to eq(916.07)
       end
     end
   end
